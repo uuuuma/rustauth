f50bb730280d5abd96e32f53d078c61e2db13e45
uuuuma <94543703+uuuuma@users.noreply.github.com>
2025-01-01 07:20:04 +0900

    refactor: use CHAR instead of VARCHAR for UUID

--- scripts/seeding/seed.sql
+++ scripts/seeding/seed.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-    id VARCHAR(36) PRIMARY KEY,
+    id CHAR(36) PRIMARY KEY,
     first_name VARCHAR(16) NOT NULL,
     last_name VARCHAR(16) NOT NULL,
     email VARCHAR(32) NOT NULL,
